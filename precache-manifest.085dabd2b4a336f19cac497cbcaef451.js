a5d62357d776b790424325776b532ad7dc8d3c34
ajay <ajay_dubey@hotmail.com>
2018-12-24 22:19:31 -0500

    Updates

---
+++ precache-manifest.085dabd2b4a336f19cac497cbcaef451.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "0974f2a2bf0e4f534e73",
+    "url": "/fusion-taste-reactjs/static/js/runtime~main.0974f2a2.js"
+  },
+  {
+    "revision": "c7e3746d28079c5297a1",
+    "url": "/fusion-taste-reactjs/static/js/main.c7e3746d.chunk.js"
+  },
+  {
+    "revision": "24ca95977a4d639d014d",
+    "url": "/fusion-taste-reactjs/static/js/1.24ca9597.chunk.js"
+  },
+  {
+    "revision": "c7e3746d28079c5297a1",
+    "url": "/fusion-taste-reactjs/static/css/main.b3e7591f.chunk.css"
+  },
+  {
+    "revision": "24ca95977a4d639d014d",
+    "url": "/fusion-taste-reactjs/static/css/1.0dc3fa8b.chunk.css"
+  },
+  {
+    "revision": "ceb687c7ece6e508e1f94d274d8c8737",
+    "url": "/fusion-taste-reactjs/index.html"
+  }
+];
