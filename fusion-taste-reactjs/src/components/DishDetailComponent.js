1be102eef4f493a6af2292cdcdee4e94fe7493b9
ajay <ajay_dubey@hotmail.com>
2018-12-30 15:17:06 -0500

    Updated presentation Components into Function Components

--- fusion-taste-reactjs/src/components/DishDetailComponent.js
+++ fusion-taste-reactjs/src/components/DishDetailComponent.js
@@ -1,69 +1,60 @@
-import React, { Component } from 'react';
+import React  from 'react';
 import { Card, CardBody, CardImg, CardText, CardTitle } from "reactstrap";
 
-class DishDetail extends Component {
-
-	constructor(props) {
-		super(props);
-		this.state = {
-			dish: null,
-		};
-	}
-	render() {
-		// selected dish will be this.prop.selectedDish
-		const selectedDish = this.props.dish;
-		return(
-			<div className="row">
-				<div  className="col-12 col-md-5 m-1">
-					{this.renderDish(selectedDish)}
-				</div>
-				<div  className="col-12 col-md-5 m-1">
-					{this.renderComments(selectedDish)}
-				</div>s
+const DishDetail = (props) => {
+	const selectedDish = props.dish;
+	return(
+		<div className="row">
+			<div  className="col-12 col-md-5 m-1">
+				{renderDish(selectedDish)}
 			</div>
-		)
-	}
+			<div  className="col-12 col-md-5 m-1">
+				{renderComments(selectedDish)}
+			</div>s
+		</div>
+	)
+};
 
-	renderComments(dish) {
-		if (dish && dish.comments != null) {
-			const formattedComments = dish.comments.map((formattedComment) => {
-				const date = new Date(formattedComment.date);
-				const formattedDate = date.toDateString();
-				return(
-					<li key={formattedComment.id}>
-						<p>{formattedComment.comment}</p>
-						<p> -- {formattedComment.author}, { formattedDate }</p>
-					</li>
-				)
-			});
+function renderComments(dish) {
+	if (dish && dish.comments != null) {
+		const formattedComments = dish.comments.map((formattedComment) => {
+			const date = new Date(formattedComment.date);
+			const formattedDate = date.toDateString();
 			return(
-				<div className="p-3">
-					<h4>Comments</h4>
-					<ul className="list-unstyled">{formattedComments}</ul>
-				</div>
-			);
-		}
-		else {
-			return(<div></div>);
-		}
+				<li key={formattedComment.id}>
+					<p>{formattedComment.comment}</p>
+					<p> -- {formattedComment.author}, { formattedDate }</p>
+				</li>
+			)
+		});
+		return(
+			<div className="p-3">
+				<h4>Comments</h4>
+				<ul className="list-unstyled">{formattedComments}</ul>
+			</div>
+		);
 	}
-
-	renderDish(dish) {
-		if (dish != null)
-			return(
-				<Card>
-					<CardImg top src={dish.image} alt={dish.name} />
-					<CardBody>
-						<CardTitle>{dish.name}</CardTitle>
-						<CardText>{dish.description}</CardText>
-					</CardBody>
-				</Card>
-			);
-		else
-			return(
-				<div></div>
-			);
+	else {
+		return(<div></div>);
 	}
 }
+
+function renderDish(dish) {
+	if (dish != null)
+		return(
+			<Card>
+				<CardImg top src={dish.image} alt={dish.name} />
+				<CardBody>
+					<CardTitle>{dish.name}</CardTitle>
+					<CardText>{dish.description}</CardText>
+				</CardBody>
+			</Card>
+		);
+	else
+		return(
+			<div></div>
+		);
+}
+
 
 export default DishDetail;
