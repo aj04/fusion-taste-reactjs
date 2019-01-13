a654bf6b60cb296a59b5bf200b28ab43affacde1
ajay <ajay_dubey@hotmail.com>
2019-01-13 16:47:17 -0500

    Added React Redux LocalForm for DishDetail Comment Submit Model Form.

--- fusion-taste-reactjs/src/components/DishDetailComponent.js
+++ fusion-taste-reactjs/src/components/DishDetailComponent.js
@@ -1,7 +1,9 @@
-import React  from 'react';
-import { Card, CardBody, CardImg, CardText, CardTitle } from "reactstrap";
+import React, { Component }  from 'react';
+import { Button,	Card,	CardBody,	CardImg, CardText, CardTitle,
+	Label, Modal, ModalBody,	ModalHeader, Row} from "reactstrap";
 import { Breadcrumb, BreadcrumbItem } from "reactstrap";
 import { Link } from "react-router-dom";
+import { Control, Errors, LocalForm } from "react-redux-form";
 
 const DishDetail = (props) => {
 	const selectedDish = props.dish;
@@ -46,6 +48,7 @@ function renderComments(comments) {
 			<div className="p-3">
 				<h4>Comments</h4>
 				<ul className="list-unstyled">{formattedComments}</ul>
+				<CommentForm/>
 			</div>
 		);
 	}
@@ -75,5 +78,77 @@ function renderDish(dish) {
 		);
 }
 
+// @todo Move CommentForm Class Component into its own File.
+class CommentForm extends Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			isModalOpen: false
+		};
+	}
+
+	toggleSubmitDialog = () => {
+		this.setState({
+			isModalOpen: !this.state.isModalOpen
+		});
+	};
+
+	handleCommentSubmit = (values) => {
+		console.log('Current State is: ' + JSON.stringify(values));
+		alert('Current State is: ' + JSON.stringify(values));
+		this.toggleSubmitDialog();
+	};
+
+	render() {
+		const required = (val) => val && val.length;
+		const maxLength = (len) => (val) => !(val) || (val.length <= len);
+		const minLength = (len) => (val) => val && (val.length >= len);
+		return (
+			<div>
+				<Modal isOpen={this.state.isModalOpen}>
+					<ModalHeader toggle={this.toggleSubmitDialog}>Submit Comment</ModalHeader>
+					<ModalBody>
+						<LocalForm onSubmit={(values) => this.handleCommentSubmit(values)}>
+							<Row className="form-group">
+								<Label htmlFor="rating">Rating</Label>
+								<Control.select model=".rating" name="rating"
+																className="form-control">
+									<option>1</option><option>2</option><option>3</option>
+									<option>4</option><option>5</option>
+								</Control.select>
+							</Row>
+							<Row className="form-group">
+								<Label htmlFor="userName">Your Name</Label>
+								<Control.text type="text" model=".userName" id="userName"
+									 name="userName" placeholder="User Name" className="form-control"
+									 validators={{ required, minLength: minLength(3), maxLength: maxLength(15) }}/>
+								<Errors
+									className="text-danger"
+									model=".userName"
+									show="touched"
+									messages={{
+										required: 'Required',
+										minLength: 'Must be greater than 2 characters',
+										maxLength: 'Must be 15 characters or less'
+									}}
+								/>
+							</Row>
+							<Row  className="form-group">
+								<Label htmlFor="comment">Comment</Label>
+								<Control.textarea model=".comment" id="comment" name="comment"
+										rows="6"	className="form-control" />
+							</Row>
+							<Button type="submit" value="submit" color="primary">Submit</Button>
+						</LocalForm>
+					</ModalBody>
+				</Modal>
+				<Button outline onClick={this.toggleSubmitDialog} >
+					<span className="fa fa-pencil fa-lg"></span>
+					Submit Comment </Button>
+			</div>
+		)
+	}
+}
 
 export default DishDetail;
